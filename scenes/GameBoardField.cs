bf09a273d7a62089d9ffd7adb10d5a7f99f6bf3a
Tamas Nemes <nemestomi2@gmail.com>
2024-02-20 23:59:38 +0100

    Add exported arrays for storing neighbors

--- scenes/GameBoardField.cs
+++ scenes/GameBoardField.cs
@@ -6,6 +6,11 @@ public partial class GameBoardField : Node2D, IField
 {
     private FieldManager _manager;
 
+    [Export]
+    public Godot.Collections.Array<int> NeighborsNormal;
+    [Export]
+    public Godot.Collections.Array<int> NeighborsKnightsOnly;
+
     public FieldManager Manager { get => _manager; set => _manager = value; }
     public List<Node2D> Pieces { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
