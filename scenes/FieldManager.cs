060e8f44190a11e57109f5865b8fe15ae50ebfae
Tamas Nemes <nemestomi2@gmail.com>
2024-02-21 01:10:36 +0100

    Correct node reference collection logic

--- scenes/FieldManager.cs
+++ scenes/FieldManager.cs
@@ -1,6 +1,4 @@
 using Godot;
-using System;
-using System.Collections.Generic;
 
 public partial class FieldManager : Node2D
 {
@@ -12,12 +10,14 @@ public partial class FieldManager : Node2D
     {
         _defaultFields = new IField[27];
         for (int i = 1; i <= 27; i++) {
-            _defaultFields[i-1] = GetNode<IField>("%" + i);
+            _defaultFields[i-1] = GetNode<GameBoardField>("%" + i);
         }
-        _safeFields[0] = GetNode<IField>("%Safe1");
-        _safeFields[1] = GetNode<IField>("%Safe2");
-        _baseFields[0] = GetNode<IField>("%Base1");
-        _baseFields[1] = GetNode<IField>("%Base2");
+        _safeFields = new IField[2];
+        _safeFields[0] = GetNode<GameBoardSafeField>("%Safe1");
+        _safeFields[1] = GetNode<GameBoardSafeField>("%Safe2");
+        _baseFields = new IField[2];
+        _baseFields[0] = GetNode<GameBoardBase>("%Base1");
+        _baseFields[1] = GetNode<GameBoardBase>("%Base2");
     }
 
     /*public List<IField> GetNeighbors(string name) {
